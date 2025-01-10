3c63e25e15f84753439b7c8d8fbbcadb90a6dcef
nino <Ninokeldishvili@gmail.com>
2025-01-10 17:38:04 +0400

    Add test should display StatsPanel when data is available

--- src/lib/components/StatsPanel.test.ts
+++ src/lib/components/StatsPanel.test.ts
@@ -1,12 +1,21 @@
 import { render, screen, waitFor } from '@testing-library/svelte';
 import { vi, describe, beforeEach, it, expect } from 'vitest';
 import StatsPanel from './StatsPanel.svelte';
+import { type GlobalStats } from '$lib/queries/fetchStats';
+
+const globalStats: GlobalStats =
+	{
+		eligibleHolders: 96,
+		totalEligibleHoldings: BigInt(173689146516658495317091n),
+		monthlyRewards:  BigInt(1000000000000000000000000n),
+		currentApy: BigInt(291724135736581994400n)
+	}
 
 vi.mock('$lib/queries/fetchStats', () => ({
 	fetchStats: vi.fn()
 }));
 
-describe('Leaderboard Component', () => {
+describe('StatsPanel Component', () => {
 	beforeEach(() => {
 		vi.clearAllMocks();
 	});
@@ -30,6 +39,19 @@ describe('Leaderboard Component', () => {
 
 		await waitFor(() => {
 			expect(screen.getByTestId('error')).toBeInTheDocument();
+		});
+	});
+
+	it('should display StatsPanel when data is available', async () => {
+		const { fetchStats } = await import('$lib/queries/fetchStats');
+		vi.mocked(fetchStats).mockResolvedValue(globalStats);
+
+		render(StatsPanel);
+
+		await waitFor(() => {
+			expect(screen.getByText('96')).toBeInTheDocument();
+			expect(screen.getByText('173689.14651666')).toBeInTheDocument();
+			expect(screen.getByText('1,000,000')).toBeInTheDocument();
 		});
 	});
 });
