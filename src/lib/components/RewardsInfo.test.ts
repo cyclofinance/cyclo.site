025d8d5bd0297e1024dfb0f506d0b6027871565d
nino <Ninokeldishvili@gmail.com>
2025-01-10 13:18:26 +0400

    Remove incorrect test

--- src/lib/components/RewardsInfo.test.ts
+++ src/lib/components/RewardsInfo.test.ts
@@ -1,20 +1,10 @@
-import { render, screen, waitFor } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
 import { describe, it, expect } from 'vitest';
 import RewardsInfo from './RewardsInfo.svelte';
-import { mockSignerAddressStore } from '$lib/mocks/mockStores';
-import Lock from '$lib/components/Lock.svelte';
 
 describe('RewardsInfo Component', () => {
 	it('should render RewardsInfo component', async () => {
 		render(RewardsInfo);
 		expect(screen.getByTestId('rewards-info')).toBeInTheDocument();
-	});
-
-	it('should show the connect message if there is no signerAddress', async () => {
-		mockSignerAddressStore.mockSetSubscribeValue('');
-		render(Lock);
-		await waitFor(() => {
-			expect(screen.getByTestId('connect-message')).toBeInTheDocument();
-		});
 	});
 });
