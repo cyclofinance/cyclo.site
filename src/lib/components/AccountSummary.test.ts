37c22537644943437acb548f69091b37ef0fb5c5
nino <Ninokeldishvili@gmail.com>
2025-01-13 15:11:43 +0400

    Add test - should display Full Tx History button

--- src/lib/components/AccountSummary.test.ts
+++ src/lib/components/AccountSummary.test.ts
@@ -1,10 +1,26 @@
 import { render, screen, waitFor } from '@testing-library/svelte';
 import { vi, describe, beforeEach, it, expect } from 'vitest';
 import AccountSummary from './AccountSummary.svelte';
+import { type PeriodStats } from '$lib/queries/fetchAccountStatus';
+import type { AccountStatusQuery } from '../../generated-graphql';
 
 vi.mock('$lib/queries/fetchAccountStatus', () => ({
 	fetchAccountStatus: vi.fn()
 }));
+
+const periodStats: PeriodStats[] = [
+	{
+		account: '0x2b462b16cb267f7545eb45829a2ce1559e56bda4',
+		netTransfers: '20261529360304309332079',
+		period: 'ALL_TIMES',
+		totalNet: '20261529360304309332079',
+		accountNet: '61529360304309332079',
+		percentage: 50,
+		proRataReward: 10
+	}
+] as unknown as PeriodStats[];
+
+let transfers: NonNullable<AccountStatusQuery['sentTransfers']> = [];
 
 describe('AccountSummary Component', () => {
 	beforeEach(() => {
@@ -19,6 +35,18 @@ describe('AccountSummary Component', () => {
 
 		await waitFor(() => {
 			expect(screen.getByTestId('loader')).toBeInTheDocument();
+		});
+	});
+
+	it('should display Full Tx History button', async () => {
+		const { fetchAccountStatus } = await import('$lib/queries/fetchAccountStatus');
+		vi.mocked(fetchAccountStatus).mockResolvedValue({ periodStats, transfers });
+
+		render(AccountSummary, { props: { account: '0x2b462b16cb267f7545eb45829a2ce1559e56bda4' } });
+
+		await waitFor(() => {
+			expect(screen.getByText('Your Rewards')).toBeInTheDocument();
+			expect(screen.getByText('Full Tx History')).toBeInTheDocument();
 		});
 	});
 });
