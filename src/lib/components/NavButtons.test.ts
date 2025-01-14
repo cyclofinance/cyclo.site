60114dd06595cf3132eea47e77354814762bcbad
nino <Ninokeldishvili@gmail.com>
2025-01-14 11:48:41 +0400

    Check app button is displayed and navigates to correct url

--- src/lib/components/NavButtons.test.ts
+++ src/lib/components/NavButtons.test.ts
@@ -1,6 +1,8 @@
-import { render, screen, waitFor } from '@testing-library/svelte';
+import { render, screen, waitFor, fireEvent } from '@testing-library/svelte';
 import { vi, describe, beforeEach, it, expect } from 'vitest';
 import NavButtons from './NavButtons.svelte';
+import { goto } from '$app/navigation';
+import { base } from '$app/paths';
 
 vi.mock('$app/navigation', () => ({
 	goto: vi.fn()
@@ -19,6 +21,21 @@ describe('NavButtons Component', () => {
 			expect(screen.getByTestId('docs-button-mobile')).toBeInTheDocument();
 			expect(screen.getByTestId('rewards-button')).toBeInTheDocument();
 			expect(screen.getByTestId('rewards-button-mobile')).toBeInTheDocument();
+		});
+	});
+
+	it('should check app button is displayed and navigates to correct url', async () => {
+		const { getByTestId } = render(NavButtons, {
+			props: { launched: true }
+		});
+
+		await waitFor(async () => {
+			expect(screen.getByTestId('app-button')).toBeInTheDocument();
+
+			const button = getByTestId('app-button');
+			await fireEvent.click(button);
+
+			expect(goto).toHaveBeenCalledWith(base + '/lock');
 		});
 	});
 });
