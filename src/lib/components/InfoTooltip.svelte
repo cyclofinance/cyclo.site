3ae9bc8e77aa26be0eb379cea0f82f36ea2b47b8
highonhopium <jks.hardy@gmail.com>
2025-04-17 20:21:07 +0100

    tooltips

---
+++ src/lib/components/InfoTooltip.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Tooltip } from 'flowbite-svelte';
+	import { InfoCircleOutline } from 'flowbite-svelte-icons';
+</script>
+
+<span
+	class="inline-block cursor-pointer text-sm text-gray-500 hover:text-gray-700"
+	data-testid="info-icon"
+>
+	<InfoCircleOutline class="h-4 w-4" color="white" />
+</span>
+<Tooltip class="max-w-[500px] font-light">
+	<slot />
+</Tooltip>
