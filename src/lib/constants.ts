90a436c737a83bf5ada6f6e10fa0728509dab5e2
rouzwelt <rouzweltt@gmail.com>
2025-08-05 16:26:32 +0000

    update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 import type { Token } from './types';
 import type { Hex } from 'viem';
 export const SUBGRAPH_URL =
-	'https://api.goldsky.com/api/public/project_clw5d06w61gg301x5435y2cso/subgraphs/test-cyclo/0.0.0/gn';
+	'https://api.goldsky.com/api/public/project_cm4zggfv2trr301whddsl9vaj/subgraphs/cyclo-rewards/0.30/gn';
 
 export const ONE = 10n ** 18n;
 
